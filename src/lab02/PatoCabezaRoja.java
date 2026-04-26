1f8e8c43cbcbd4a3f45cafe700971f03625919eb
USUARIO <USUARIO@DESKTOP-D4TUHQ0>
2026-04-26 00:47:15 -0500

    CREE LA CLASE PatoCabezaRoja

---
+++ src/lab02/PatoCabezaRoja.java
@@ -0,0 +1,14 @@
+package lab02;
+
+public class PatoCabezaRoja extends Pato {
+
+    public void PatoCabezaRoja() {
+        volable = new VuelaConAlas();
+        cuaqueable = new Cuaqueo();
+    }
+
+    @Override
+    public void mostrar() {
+        System.out.println("Yo soy un real PATO CABEZA ROJA");
+    }
+}
