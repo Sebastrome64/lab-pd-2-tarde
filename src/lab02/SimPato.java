e18ccaa28cd9694d04ab35add0667fbfd1c5a3be
USUARIO <USUARIO@DESKTOP-D4TUHQ0>
2026-04-26 01:26:48 -0500

    HICE EL MAIN SIMPATO

--- src/lab02/SimPato.java
+++ src/lab02/SimPato.java
@@ -1,11 +1,37 @@
-
 package lab02;
 
 public class SimPato {
 
     public static void main(String[] args) {
-        
+
+        Pato patoReal = new PatoReal();
+        patoReal.mostrar();
+        patoReal.realizarCuaqueo();
+        patoReal.realizarVuelo();
+        patoReal.nadar();
+
+        System.out.println();
+
+        Pato patoGoma = new PatoDeGoma();
+        patoGoma.mostrar();
+        patoGoma.realizarCuaqueo();
+        patoGoma.realizarVuelo();
+        patoGoma.nadar();
+
+        System.out.println();
+
+        Pato patoSenuelo = new PatoSenuelo();
+        patoSenuelo.mostrar();
+        patoSenuelo.realizarCuaqueo();
+        patoSenuelo.realizarVuelo();
+        patoSenuelo.nadar();
+
+        System.out.println();
+
+        Pato patoCabezaRoja = new PatoCabezaRoja();
+        patoCabezaRoja.mostrar();
+        patoCabezaRoja.realizarCuaqueo();
+        patoCabezaRoja.realizarVuelo();
+        patoCabezaRoja.nadar();
     }
-    
-    
 }
