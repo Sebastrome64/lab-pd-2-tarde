7f812cd23fdd889902ed4ca0ac978db1aa246e78
USUARIO <USUARIO@DESKTOP-D4TUHQ0>
2026-04-26 00:53:45 -0500

    CREE LA CLASE PatoDeGoma

--- src/lab02/PatoDeGoma.java
+++ src/lab02/PatoDeGoma.java
@@ -1,13 +1,15 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package lab02;
 
-/**
- *
- * @author sistemas
- */
-public class PatoDeGoma {
-    
+public class PatoDeGoma extends Pato {
+
+    public void PatoDeGoma() {
+        volable = new NoVuela();
+        cuaqueable = new Chirreo();
+    }
+
+    @Override
+    public void mostrar() {
+        System.out.println("Yo soy un PATITO DE GOMA");
+
+    }
 }
